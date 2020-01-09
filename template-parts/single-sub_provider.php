6bffc4ae99b935209b2202163dc896039ea7551c
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-01-09 13:46:47 +0100

    Switching to single-sub-provider.php

--- template-parts/single-sub_provider.php
+++ template-parts/single-sub_provider.php
@@ -9,6 +9,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<?php echo "HAAAAA" ?>
 
 			<?php if ( has_post_format( array( 'gallery', 'video', 'image' ) ) ) {
 				get_template_part( 'template-parts/content', get_post_format() );
