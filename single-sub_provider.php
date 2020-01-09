0dfd769a91a8ac9f7f33448136b9ad4a87ed3ecc
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-01-09 13:39:34 +0100

    Switching to single-sub-provider.php

--- single-sub_provider.php
+++ single-sub_provider.php
@@ -10,9 +10,6 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
-			<p>alsdfjlasdfjasdlkf</p>
-
 			<?php if ( has_post_format( array( 'gallery', 'video', 'image' ) ) ) {
 				get_template_part( 'template-parts/content', get_post_format() );
 			} else {
